b289d2ca062c1fc2b24d80aca825b5db94463d60
xiao9878 <55609365>
2020-11-17 11:23:26 +0800

    初始化提交

---
+++ res/response.go
@@ -0,0 +1,39 @@
+package res
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type ResponseData struct {
+	Code ResCode     `json:"code"`
+	Msg  interface{} `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func Error(c *gin.Context, code ResCode) {
+	rd := &ResponseData{
+		Code: code,
+		Msg:  code.Msg(),
+		Data: nil,
+	}
+	c.JSON(http.StatusOK, rd)
+}
+
+func ErrorWithMsg(c *gin.Context, code ResCode, msg interface{}) {
+	rd := &ResponseData{
+		Code: code,
+		Msg:  code.Msg(),
+		Data: nil,
+	}
+	c.JSON(http.StatusOK, rd)
+}
+
+func Success(c *gin.Context, data interface{}) {
+	rd := &ResponseData{
+		Code: CodeSuccess,
+		Msg:  CodeSuccess.Msg(),
+		Data: data,
+	}
+	c.JSON(http.StatusOK, rd)
+}
